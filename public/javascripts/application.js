600119d0f5d61d7c23c68516b6255c8d54b9436f
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-08-31 05:22:42 -0500

    Search input clears itself

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
 $(document).ready(function(){
  
+ var searchBox = jQuery("input#terms"); 
+ var defaultValue = searchBox.val(); 
+ 
+  searchBox.click(function() {
+  if( this.value == defaultValue ) {
+  jQuery(this).val("");
+  }
+  });
+  searchBox.blur(function() {
+  if(jQuery.trim(this.value) == "") {
+  jQuery(this).val(defaultValue);
+  }
+  });
+
+
 $("a.tip").click(function() {
 
        $(this).next("div.top").stop(true, true).animate({opacity: "show", top: "-105"}, "slow");
@@ -12,6 +28,8 @@ $("a.tip").click(function() {
 
       return false;
   });
+
+
 
 });
 
