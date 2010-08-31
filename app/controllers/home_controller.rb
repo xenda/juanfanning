7f111f7b9fa9d80ac9c28ca959fdd57232488cfd
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-08-31 05:54:25 -0500

    Users can filter how many records they'd like on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     before_filter :authenticate_user!
   
   def index
-    @projects = Project.published.paginate(:page => params[:page])
+    @projects = Project.published.paginate(:page => params[:page], :per_page => params[:per_page])
   end
   
   def search
