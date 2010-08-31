4ece57df418ca49f2d6d6a4debb42c1d7aecf39d
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-08-31 06:41:09 -0500

    Updated with models

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,7 +6,7 @@ class Search
   RESULT_TYPES = { "Both" => "both", "Preliminaries only" => "preliminaries", "Finals Only" => "finals"}
   TIMEFRAMES = { "Past day" => "day", "Past week" => "week", "Past month" => "month", "Past year" => "year"}
   
-  SORT_LIST
-  SORT_TYPES
+  SORT_LIST = { "By date" => "date", "Alphabetically" => "alpha"}
+  SORT_TYPES = { "Descending" => "desc", "Ascending" => "asc"  }
   
 end
