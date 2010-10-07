404879dac40bdd378fa5f78a34963ad8b48f0bb1
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-10-07 08:00:24 -0500

    Non-existant pages will redirect to root

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,13 @@ class PagesController < InheritedResources::Base
   def show
     @page = Page.find_by_slug(params[:id])
     @page = Page.find_by_id(params[:id]) unless @page
-    @title = @page.head_title
-    @tags = @page.metatags
-    show!
+    if @page
+      @title = @page.head_title
+      @tags = @page.metatags
+      show!
+    else
+      redirect_to root_path
+    end
   end
 
 end
